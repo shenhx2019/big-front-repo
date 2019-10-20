cdefec27eee819e78829092a6d868a070b29238e
shenhx <623724489@qq.com>
2019-10-20 10:37:39 +0800

    引入commander和color等组件

--- src/02typescript/02outgo-weather/src/index.ts
+++ src/02typescript/02outgo-weather/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello,ts-weather')
+// console.log('hello,ts-weather')
+import commander from 'commander';
+import colors from 'colors';
+
+const command = commander
+    .version("0.1.0")
+    .option("-c, --city [name]", "Add city name")
+    .parse(process.argv);
+if(process.argv.slice(2).length === 0){
+    command.outputHelp(colors.red);
+    process.exit();
+}
+if(!command.city){
+    command.outputHelp();
+}
+console.log(command.city);
